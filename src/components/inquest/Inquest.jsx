53a5f020affe1697cc6e69271eca6ce075707c0f
JN <jared.bozh@gmail.com>
2023-05-28 00:51:48 +0800

    logic

--- src/components/inquest/Inquest.jsx
+++ src/components/inquest/Inquest.jsx
@@ -4,68 +4,100 @@ import './inquest.scss'
 
 
 const Inquest = () => {
-
-	const [counter, setCounter] = React.useState(['start'])
-	const [state, setState] = React.useState({
+	const startState = {
 		current: 'start',
 		textContent: [['START', newQuestion], ['START', newQuestion], ['START', newQuestion]],
 		styleContainer: ['op_0', ' ', 'op_0'],
 		pictureCount: 1,
 	}
-	)
+	const [counter, setCounter] = React.useState(['start'])
+	const [state, setState] = React.useState({ ...startState })
+
+
 
 	React.useEffect(
 		() => {
-			let newCounter = [...counter, state.current]
 
-			setCounter(newCounter)
-
+			if (!counter.includes(state.current)) {
+				let newCounter = [...counter, state.current]
+				setCounter(newCounter)
+			}
+			// console.log(counter)
 		}, [state.current]
 	)
 
 	function newQuestion() {
-		console.log('NEW QUESTION')
-
-		let random = Math.floor(Math.random() * 3);
-		const nextQuest = question[random]
-		console.log('Random value:' + random)
+		console.log(this)
+		const nextQuest = question[getRandomQuestion(this.counter, this.state)]
 
 		setState({
 			...nextQuest,
 		})
 	}
+	function win() {
+		(this.counter.length == 4)
+			? setState({ ...promocode, })
+			: newQuestion.bind({ ...this })()
+	}
+	function lose() {
 
+		setCounter(['start'])
+		setState({
+			...startState,
+		})
+	}
+
+	const getRandomQuestion = (counter, state) => {
+		if (counter.length < 4) {
+			let random = Math.floor(Math.random() * 3)
+			while (counter.includes(`q${random}`)) {
+				random = Math.floor(Math.random() * 3)
+			}
+			console.log(random)
+			return random
+		}
+
+		return state.current.split('')[1]
+	}
 
 
 	const question = [
 		{
-			current: 'q1',
-			textContent: [['q1Ответ1', newQuestion], ['q1Ответ2', newQuestion], ['q1Ответ3', newQuestion]],
+			current: 'q0',
+			textContent: [['q0Ответ1', win], ['q0Ответ2', lose], ['q0твет0', lose]],
 			styleContainer: [' ', ' ', ' '],
 			pictureCount: 1,
 		},
 		{
-			current: 'q2',
-			textContent: [['q2Ответ1', newQuestion], ['q2Ответ2', newQuestion], ['q2Ответ3', newQuestion]],
+			current: 'q1',
+			textContent: [['q1Ответ1', win], ['q1Ответ2', lose], ['q1Ответ3', lose]],
 			styleContainer: [' ', ' ', ' '],
 			pictureCount: 2,
 		},
 		{
-			current: 'q3',
-			textContent: [['q3Ответ1', newQuestion], ['q3Ответ2', newQuestion], ['q3Ответ3', newQuestion]],
+			current: 'q2',
+			textContent: [['q2Ответ1', win], ['q2Ответ2', lose], ['q2Ответ3', lose]],
 			styleContainer: [' ', ' ', ' '],
 			pictureCount: 3,
 		},
 	]
+	const promocode = {
+		current: 'promo',
+		textContent: [['', () => { }], ['ПОЗДРАВЛЯЕМ', () => { }], ['', () => { }]],
+		styleContainer: [' op_0', ' ', ' op_0'],
+		pictureCount: 1,
+	}
 
 
 
 	const getTextForButton = (index) => { return <span>{state.textContent[index][0]}</span> }
-	const getClickEventHandler = (index) => { return state.textContent[index][1] }
+	const getClickEventHandler = (index) => {
+		return state.textContent[index][1].bind({ state, counter })
+	}
 	const getStyleForButton = (index) => {
 		return `slide__button ${state.styleContainer[index]}`
 	}
-	const getStateCounter = () => { return counter.length }
+	const getStateCounter = () => { return (counter.length - 1) }
 
 	return (
 		<div className='inquest__wrapper'>
@@ -79,13 +111,13 @@ const Inquest = () => {
 				</div>
 				<div className="slide__buttons">
 
-					<div className={getStyleForButton(0)} onClick={getClickEventHandler(0)}>
+					<div className={getStyleForButton(0)} onClick={getClickEventHandler(0).bind({ state, counter })}>
 						{getTextForButton(0)}
 					</div>
-					<div className={getStyleForButton(1)} onClick={getClickEventHandler(1)}>
+					<div className={getStyleForButton(1)} onClick={getClickEventHandler(1).bind({ state, counter })}>
 						{getTextForButton(1)}
 					</div>
-					<div className={getStyleForButton(2)} onClick={getClickEventHandler(2)}>
+					<div className={getStyleForButton(2)} onClick={getClickEventHandler(2).bind({ state, counter })}>
 						{getTextForButton(2)}
 					</div>
 
