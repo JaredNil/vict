8fab3140fe5a282da0d20d586b51c9f378b485ac
JN <jared.bozh@gmail.com>
2023-05-28 19:54:02 +0800

    photo

---
+++ src/components/ds.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+
+import ham1 from '../../resources/ham/ham1.png'
+import ham2 from '../../resources/ham/ham2.png'
+
+import cheese1 from '../../resources/cheese/cheese1.png'
+import cheese2 from '../../resources/cheese/cheese2.png'
+import cheese3 from '../../resources/cheese/cheese3.png'
+
+import hotdog1 from '../../resources/hotdog/hotdog1.png'
+import hotdog2 from '../../resources/hotdog/hotdog2.png'
+
+import chicken1 from '../../resources/chicken/chicken1.png'
+import chicken2 from '../../resources/chicken/chicken2.png'
+import chicken3 from '../../resources/chicken/chicken3.png'
+
+import cesar from '../../resources/cesar/cesar.png'
+
+import italic1 from '../../resources/italic/italic1.png'
+import italic2 from '../../resources/italic/italic2.png'
+
+import cheeseChicken from '../../resources/cheeseChicken/cheeseChicken.png'
+
+import carbonara1 from '../../resources/carbonara/carbonara1.png'
+import carbonara2 from '../../resources/carbonara/carbonara2.png'
+import carbonara3 from '../../resources/carbonara/carbonara3.png'
+
+import tomat1 from '../../resources/tomat/tomat1.png'
+import tomat2 from '../../resources/tomat/tomat2.png'
+import tomat3 from '../../resources/tomat/tomat3.png'
+
+import greek1 from '../../resources/greek/greek1.png'
+import greek2 from '../../resources/greek/greek2.png'
+
+import four from '../../resources/four/four.png'
+
+import season from '../../resources/season/season.png'
+
+
+const ds = () => {
+	return (
+		<div>
+
+		</div>
+	);
+};
+
+export default ds;
--- src/components/inquest/Inquest.jsx
+++ src/components/inquest/Inquest.jsx
@@ -10,12 +10,48 @@ import america1 from '../../resources/america/america1.png'
 import america2 from '../../resources/america/america2.png'
 import america3 from '../../resources/america/america3.png'
 
+import ham1 from '../../resources/ham/ham1.png'
+import ham2 from '../../resources/ham/ham2.png'
+
+import cheese1 from '../../resources/cheese/cheese1.png'
+import cheese2 from '../../resources/cheese/cheese2.png'
+import cheese3 from '../../resources/cheese/cheese3.png'
+
+import hotdog1 from '../../resources/hotdog/hotdog1.png'
+import hotdog2 from '../../resources/hotdog/hotdog2.png'
+
+import chicken1 from '../../resources/chicken/chicken1.png'
+import chicken2 from '../../resources/chicken/chicken2.png'
+import chicken3 from '../../resources/chicken/chicken3.png'
+
+import cesar from '../../resources/cesar/cesar.png'
+
+import italic1 from '../../resources/italic/italic1.png'
+import italic2 from '../../resources/italic/italic2.png'
+
+import cheeseChicken from '../../resources/cheeseChicken/cheeseChicken.png'
+
+import carbonara1 from '../../resources/carbonara/carbonara1.png'
+import carbonara2 from '../../resources/carbonara/carbonara2.png'
+import carbonara3 from '../../resources/carbonara/carbonara3.png'
+
+import tomat1 from '../../resources/tomat/tomat1.png'
+import tomat2 from '../../resources/tomat/tomat2.png'
+import tomat3 from '../../resources/tomat/tomat3.png'
+
+import greek1 from '../../resources/greek/greek1.png'
+import greek2 from '../../resources/greek/greek2.png'
+
+import four from '../../resources/four/four.png'
+
+import season from '../../resources/season/season.png'
+
 
 const Inquest = () => {
 	const startState = {
 		current: 'start',
 		textContent: [['START', newQuestion], ['START', newQuestion], ['START', newQuestion]],
-		styleContainer: ['op_0', ' ', 'op_0'],
+		styleContainer: ['op_0', ' start', 'op_0'],
 		pictureCount: 1,
 	}
 	const [counter, setCounter] = React.useState(['start'])
@@ -43,7 +79,7 @@ const Inquest = () => {
 		})
 	}
 	function win() {
-		(this.counter.length == 4)
+		(this.counter.length == 15)
 			? setState({ ...promocode, })
 			: newQuestion.bind({ ...this })()
 	}
@@ -58,10 +94,10 @@ const Inquest = () => {
 	}
 
 	const getRandomQuestion = (counter, state) => {
-		if (counter.length < 4) {
-			let random = Math.floor(Math.random() * 3)
+		if (counter.length < 15) {
+			let random = Math.floor(Math.random() * 15)
 			while (counter.includes(`q${random}`)) {
-				random = Math.floor(Math.random() * 3)
+				random = Math.floor(Math.random() * 15)
 			}
 			console.log(random)
 			return random
@@ -75,7 +111,7 @@ const Inquest = () => {
 		{
 			current: 'q0',
 			text: 'Гавай',
-			textContent: [['Гавайская', win], ['Пепперони и томаты', lose], ['Чикен Спайси', lose]],
+			textContent: [['Гавайская', win], ['Пепперони', lose], ['Чикен Спайси', lose]],
 			styleContainer: [' ', ' ', ' '],
 			pictureCount: 1,
 			pictureStorage: [gavai1]
@@ -83,7 +119,7 @@ const Inquest = () => {
 		{
 			current: 'q1',
 			text: 'Терияки',
-			textContent: [['Терияки', win], ['Сырный', lose], ['Камамбер', lose]],
+			textContent: [['Сырный', lose], ['Камамбер', lose], ['Терияки', win]],
 			styleContainer: [' ', ' ', ' '],
 			pictureCount: 2,
 			pictureStorage: [teriyaki1, teriyaki2]
@@ -91,10 +127,114 @@ const Inquest = () => {
 		{
 			current: 'q2',
 			text: 'Американо',
-			textContent: [['Американо', win], ['Гавайская', lose], ['Греческая', lose]],
+			textContent: [['Гавайская', lose], ['Американо', win], ['Греческая', lose]],
 			styleContainer: [' ', ' ', ' '],
 			pictureCount: 3,
 			pictureStorage: [america1, america2, america3]
+		},
+		{
+			current: 'q3',
+			text: 'Ветчина',
+			textContent: [['Бьянко', lose], ['Ветчина и сыр', win], ['Морская', lose]],
+			styleContainer: [' ', ' ', ' '],
+			pictureCount: 2,
+			pictureStorage: [ham1, ham2]
+		},
+		{
+			current: 'q4',
+			text: 'Сырная',
+			textContent: [['Пепперони', lose], ['Сальса', lose], ['Сырная', win]],
+			styleContainer: [' ', ' ', ' '],
+			pictureCount: 2,
+			pictureStorage: [ham1, ham2]
+		},
+		{
+			current: 'q5',
+			text: 'Сырная',
+			textContent: [['Ветчина и сыр', lose], ['Гавайская', lose], ['Сырная', win]],
+			styleContainer: [' ', ' ', ' '],
+			pictureCount: 3,
+			pictureStorage: [cheese1, cheese2, cheese3]
+		},
+		{
+			current: 'q5',
+			text: 'Хотдог',
+			textContent: [['Хотдог', win], ['Чикендог', lose], ['Хоткэт', lose]],
+			styleContainer: [' ', ' ', ' '],
+			pictureCount: 2,
+			pictureStorage: [hotdog1, hotdog2]
+		},
+		{
+			current: 'q6',
+			text: 'Куриная',
+			textContent: [['Сырная', lose], ['Куриная', win], ['Пепперони', lose]],
+			styleContainer: [' ', ' ', ' '],
+			pictureCount: 3,
+			pictureStorage: [chicken1, chicken2, chicken3]
+		},
+		{
+			current: 'q7',
+			text: 'Цезарь',
+			textContent: [['Черепаха', lose], ['Зимний', lose], ['Цезарь', win]],
+			styleContainer: [' ', ' ', ' '],
+			pictureCount: 1,
+			pictureStorage: [cesar]
+		},
+		{
+			current: 'q8',
+			text: 'Итальянская',
+			textContent: [['Мясная', lose], ['Итальянская', win], ['Мексиканская', lose]],
+			styleContainer: [' ', ' ', ' '],
+			pictureCount: 2,
+			pictureStorage: [italic1, italic2]
+		},
+		{
+			current: 'q9',
+			text: 'Сырный цыплёнок',
+			textContent: [['Сырный цыплёнок', win], ['Томатный цыпленок', lose], ['Сальса', lose]],
+			styleContainer: [' ', ' ', ' '],
+			pictureCount: 1,
+			pictureStorage: [cheeseChicken]
+		},
+		{
+			current: 'q10',
+			text: 'Карбонара',
+			textContent: [['Фишролл', lose], ['Чизкейк', lose], ['Карбонара', win]],
+			styleContainer: [' ', ' ', ' '],
+			pictureCount: 3,
+			pictureStorage: [carbonara1, carbonara2, carbonara3]
+		},
+		{
+			current: 'q11',
+			text: 'Томатная',
+			textContent: [['Дон Бекон', lose], ['Томатная', win], ['Мексиканская', lose]],
+			styleContainer: [' ', ' ', ' '],
+			pictureCount: 3,
+			pictureStorage: [tomat1, tomat2, tomat3]
+		},
+		{
+			current: 'q12',
+			text: 'Греческая',
+			textContent: [['Римская', lose], ['Греческая', win], ['Португальская', lose]],
+			styleContainer: [' ', ' ', ' '],
+			pictureCount: 2,
+			pictureStorage: [greek1, greek2]
+		},
+		{
+			current: 'q13',
+			text: '4 сыра',
+			textContent: [['4 мира', lose], ['4 сыра', win], ['4 стороны света', lose]],
+			styleContainer: [' ', ' ', ' '],
+			pictureCount: 1,
+			pictureStorage: [four]
+		},
+		{
+			current: 'q14',
+			text: '4 сезона',
+			textContent: [['4 сезона', win], ['Додо', win], ['Цыпленок', lose]],
+			styleContainer: [' ', ' ', ' '],
+			pictureCount: 1,
+			pictureStorage: [season]
 		},
 	]
 
@@ -103,6 +243,7 @@ const Inquest = () => {
 		textContent: [['', () => { }], ['ПОЗДРАВЛЯЕМ', () => { }], ['', () => { }]],
 		styleContainer: [' op_0', ' ', ' op_0'],
 		pictureCount: 1,
+		pictureStorage: [america1]
 	}
 
 
@@ -111,7 +252,7 @@ const Inquest = () => {
 
 		setState({
 			...state.state,
-			styleContainer: ['red', 'red', 'red']
+			styleContainer: ['redBack', 'redBack', 'redBack']
 		})
 	}
 
@@ -125,7 +266,7 @@ const Inquest = () => {
 		return `slide__button ${state.styleContainer[index]}`
 	}
 	const getStateCounter = () => {
-		return (counter.length == 5) ? 3 : (counter.length - 1)
+		return (counter.length == 14) ? 14 : (counter.length - 1)
 	}
 	const getTotalPicture = () => {
 		return state.pictureCount
@@ -153,8 +294,8 @@ const Inquest = () => {
 
 			<div className='slide'>
 				<div className={`slide__counter`}>
-					<span id='count' className={([1, 2, 3].includes(getStateCounter())) ? '' : 'hide'}>
-						{getStateCounter()}/3
+					<span id='count' className={([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].includes(getStateCounter())) ? '' : 'hide'}>
+						{getStateCounter()}/15
 					</span>
 				</div>
 				<div className={`slide__picture ${getTotalPictureToClass()}`}>
